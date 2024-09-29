4d735e35a065775c19381f59cecf324358ae2909
rohannstu <rohansinha141635@gmail.com>
2024-09-29 21:57:20 +0600

    first commit

---
+++ src/main/java/org/packages/movieverse/MovieVerseApplication.java
@@ -0,0 +1,13 @@
+package org.packages.movieverse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieVerseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MovieVerseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/packages/movieverse/MovieVerseApplicationTests.java
@@ -0,0 +1,13 @@
+package org.packages.movieverse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieVerseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
