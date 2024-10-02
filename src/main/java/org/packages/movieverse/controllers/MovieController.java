13782506846e749d7da0f7db8c37478f93590bac
rohannstu <rohansinha141635@gmail.com>
2024-10-03 01:59:46 +0600

    liquibase added

--- src/main/java/org/packages/movieverse/controllers/MovieController.java
+++ src/main/java/org/packages/movieverse/controllers/MovieController.java
@@ -9,7 +9,6 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
-import org.springframework.web.server.ResponseStatusException;
 
 import java.io.IOException;
 import java.util.List;
